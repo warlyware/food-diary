b459610548a55ae563283da77469525213f4ab09
warlyware <danward@gmail.com>
2015-07-01 01:35:46 -0700

    Add basic BMI calculations, user delete

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -6,29 +6,67 @@ app.controller("MainCtrl", function($scope, $http, User, Food) {
   $scope.showUserForm = function() {
     console.log('Angular!');
   }
-  $scope.saveFood = function(user, food) {
-    console.log('user: ' + user.age, 'food: ' + food);
+
+  $scope.deleteEntry = function(index) {
+    console.log(index);
+    $scope.foods.splice(index, 1)
+  }
+
+  var editingIndex;
+  var editing = false;
+  $scope.editEntry = function(index) {
+    console.log(index);
+    $scope.food = $scope.foods[index];
+    editing = true;
+    editingIndex = index;
+  }
+
+  var foodsArray = [];
+  var usersArray = [];
+
+  $scope.saveUser = function(user) {
     var newUser = new User(user);
-    var newFood = new Food(food);
-    console.log(newUser);
-    console.log(newFood);
+    usersArray.push(newUser); 
     $scope.user = newUser;
-    $scope.foods = newFood;
     $http.post("/user", {user: newUser}).success(function(data) {
       console.log(data);
+    }).catch(function(err) {
+      console.log(err);
+    });
+  }
+
+  $scope.saveFood = function(food) {
+    var newFood = new Food(food);
+    foodsArray.push(newFood);
+    console.log(editing);
+    // if (editing === true) {
+    //   $scope.foods[editingIndex] = food;
+    $scope.foods = foodsArray;
+
+    computeWeight();
+    computeBMI();
+
+    $http.post("/food", {food: newFood}).success(function(data) {
+      console.log(data);
+      $scope.food = [];
       // $scope.quotes.push(data.quote);
       // $scope.newQuote = "";
     }).catch(function(err) {
       console.log(err);
     });
+  }
 
-    $http.post("/food", {user: newFood}).success(function(data) {
-      console.log(data);
-      // $scope.quotes.push(data.quote);
-      // $scope.newQuote = "";
-    }).catch(function(err) {
-      console.log(err);
-    });    
+  function computeWeight() {
+    var totalCalories = $scope.foods.reduce(function(accumulator, food) {
+      return accumulator + (food.calories * food.servings);
+    }, 0);
+    $scope.weightGained = totalCalories / 3500;
+    $scope.currentWeight = $scope.user.weight + $scope.weightGained;
+  }
+
+  function computeBMI() {
+    var inches = $scope.user.height;
+    $scope.bmi = ($scope.user.weight * 703) / Math.pow(inches, 2);
   }
 });
 
@@ -49,11 +87,14 @@ app.factory("User", function() {
 
 app.factory("Food", function() {
   // Define the User function
+  var today = new Date();
+  var todayFormatted = today.toLocaleDateString();
   var Food = function(food) {
     this.desc = food.desc;
     this.meal = food.meal;
     this.calories = food.calories;
     this.servings = food.servings;
+    this.date = todayFormatted;
   };
  
   // Return a reference to the function
